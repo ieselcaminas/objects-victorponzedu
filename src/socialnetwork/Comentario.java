c4c28f8f0fa295d1c739caad5d0514b4babe92e7
victor ponz <victorponz@gmail.com>
2025-01-28 12:25:39 +0100

    Añadido Comentario

---
+++ src/socialnetwork/Comentario.java
@@ -0,0 +1,41 @@
+package socialnetwork;
+
+public class Comentario {
+    private int id;
+    private Usuario usuario;
+    private String texto;
+    private Publicacion publicacion;
+
+    public Comentario(int id, Usuario usuario, String texto, Publicacion publicacion) {
+        this.id = id;
+        this.usuario = usuario;
+        this.texto = texto;
+        this.publicacion = publicacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Publicacion getPublicacion() {
+        return publicacion;
+    }
+
+    @Override
+    public String toString(){
+        return "id -" + this.id + "Usuario - " + this.usuario.getNombre() + " texto: " + this.texto;
+    }
+}
--- src/socialnetwork/Main.java
+++ src/socialnetwork/Main.java
@@ -10,27 +10,34 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Publicacion> publicaciones = new ArrayList<>();
+        List<Comentario> comentarios = new ArrayList<>();
 
         Usuario juan = new Usuario("juan", "Juan Pérez", 1);
         Mensaje t = new Mensaje(juan, 1, "Primer mensaje");
         publicaciones.add(t);
-        System.out.println(t);
+        //System.out.println(t);
 
         Usuario maria = new Usuario("maria", "María Pérez", 1);
         Fotografia f = new Fotografia(maria, "Amanecer", "c:/amanecer.png");
         publicaciones.add(f);
+        Comentario c = new Comentario(1, juan, "Muy bueno", t);
+        comentarios.add(c);
+
+        for(Comentario comentario: comentarios){
+            System.out.println(comentario.getPublicacion().getUsuario().getNick());
+        }
 
         f.darLike();
-        System.out.println(f);
+        //System.out.println(f);
 
 
 
-        System.out.println(f.getUsuario().getNick());
+        //System.out.println(f.getUsuario().getNick());
 
     }
     private static void publicacionesDe(String nombre, List<Publicacion> listaPubicaciones){
         listaPubicaciones.stream()
-                .filter((p) -> p.getUsuario().getNombre().contains(nombre))
+                .filter((p) -> p.getUsuario().getNombre().equals(nombre))
                 .forEach(System.out::println);
     }
 }
